91b44bbd786badeb41bc4f731020d89322360c23
NASCOMMS Pty Ltd <graham@nascomms.com>
2017-03-05 17:01:45 +1000

    Initial Commit

---
+++ emoji learn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  emoji learn
+//
+//  Created by Graham Acworth on 5/3/17.
+//  Copyright © 2017 Nascomms. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
